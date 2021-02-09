e1d8b9b2faa60fe840fdb2105796f02db723ea10
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-02-09 12:08:04 -0800

    initial setup

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <ul>
+                <li>Home</li>
+                <li>Portfolio</li>
+                <li>About</li>
+            </ul>
+            <h1>Stephen Ibanez</h1>
+        </div>
+    );
+}
+
+export default Header;

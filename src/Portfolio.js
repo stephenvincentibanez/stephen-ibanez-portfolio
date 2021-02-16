fecc0349f5657f3bd0c866a86c63c65abda88f96
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-02-16 14:13:52 -0800

    blog mobile, portfolio mobile, fix portfolio desktop

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -4,40 +4,10 @@ import './Portfolio.css'
 import db from './firebase';
 
 const Portfolio = () => {
-    // const [projects, setProjects] = useState([]);
-
-    // useEffect(() => {
-    //     db.collection('projects').onSnapshot((snapshot) => {
-    //             setProjects(
-    //                 snapshot.docs.map((doc) => (
-    //                     {
-    //                         projectName: doc.data().projectName,
-    //                         projectUrl: doc.data().projectUrl,
-    //                         projectImg: doc.data().projectImg
-    //                     }
-    //                 ))
-    //             )
-    //         })
-    // }, [])
 
     return (
         <div className="portfolio">
-             {/* <img
-                className='portfolio__background' 
-                src='https://venngage-wordpress.s3.amazonaws.com/uploads/2018/09/Colorful-Geometric-Simple-Background-Image.jpg' 
-                alt=''/> */}
             <div className='portfolio__projects'>
-                {/* {
-                    projects.map(project => (
-                        <div className='portfolio__project'>
-                            <Project 
-                                projectName={project.projectName} 
-                                projectImg={project.projectImg} 
-                                projectUrl={project.projecUrl}/>
-                        </div>
-
-                    ))
-                } */}
                 <div className='portfolio__project'>
                     <Project 
                         projectName='ApartmentFindr' 
